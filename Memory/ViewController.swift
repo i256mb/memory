85ba7fd9de3090304ad207201c047d2527c5b335
i256mb <48620302+i256mb@users.noreply.github.com>
2019-04-03 00:28:55 +0500

    Add files via upload

---
+++ Memory/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  Memory
+//
+//  Created by Maxim Vitovitsky on 29/03/2019.
+//  Copyright © 2019 Максим Витовицкий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    lazy var game = Memory(numberOfPairsOfCrads: (cardButtons.count + 1) / 2)
+    
+    var gameTimer: Timer!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func cardButtonAction(_ sender: UIButton) {
+        if let cardIndex = cardButtons.firstIndex(of: sender) {
+            game.chooseCard(at: cardIndex)
+            updateButtons()
+        } else {
+            print("This button is not in card buttons!")
+        }
+    }
+    
+    func updateButtons() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: .normal)
+                button.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            } else {
+                button.setTitle("", for: .normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 0.2723996043, green: 0.6819463372, blue: 0.632582128, alpha: 1)
+            }
+        }
+        gameTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(hide_right_answer), userInfo: nil, repeats: false)
+    }
+    
+    var emojiList = ["🦊", "🐻", "🐼", "🐨", "🦁", "🐯", "🐵", "🦉", "🦇"]
+    var emoji = [Int:String]()
+    
+    func emoji(for card: Card) -> String {
+        if emoji[card.id] == nil, emojiList.count > 0 {
+            let randomIndex = Int.random(in: 0..<emojiList.count)
+            emoji[card.id] = emojiList.remove(at: randomIndex)
+        }
+        
+        return emoji[card.id] ?? "?"
+    }
+
+    @IBOutlet weak var butt_nothing: UIButton!
+    
+ 
+    
+    @objc func hide_right_answer(){
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isMatched {
+                button.setTitle("", for: .normal)
+                button.backgroundColor =  #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
+            }
+        }
+    }
+    
+    
+    @IBAction func resetGame(_ sender: UIButton) {
+        fReset()
+    }
+    
+    
+    
+    func fReset(){
+        game.cards.removeAll()
+        Card.lastIdentifier = -1
+        game = Memory(numberOfPairsOfCrads: (cardButtons.count + 1) / 2)
+        updateButtons()
+    }
+}
+
